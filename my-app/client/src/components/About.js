30bc5a140940676ae14773d5b5d3e4285816c355
Carlo Ortega <cfor0@outlook.com>
2020-01-23 17:52:30 -0500

    changed files and added contact

---
+++ my-app/client/src/components/About.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+    Box,
+    Image,
+    Heading,
+    Text,
+    Flex
+} from 'rebass';
+import linkPic from '../assets/images/linkPic.jpg'
+
+const aboutStyle = {
+    fontSize: 16,   
+}
+function About() {
+    return (
+        <Flex flexWrap='wrap' mx={2} my={2} p={4}>
+
+            <Box width={1/4} paddingTop={5}>
+                <Image sx={{
+                    borderRadius: 8,
+                }} src={linkPic} />
+            </Box>
+
+            <Box paddingLeft={['5%']} paddingRight={['5%']} marginTop={40} width={3/4} style={aboutStyle} >
+                <Heading fontSize={[4, 5, 6]} textAlign='center'>About Me</Heading>
+                <br></br>
+                <Text >Most of my life has been dedicated to playing music, until I wrote my first line of code.<br></br>
+                    I love the concept of functionality and controlling outputs of software applications. Since I have learned to code, I have felt a sense of freedom and have not stopped learning in this field. There is just something special about using logic to problem-solve errors that makes me passionate about Software Development. Coding is like music, there is a certain way of thinking to fully grasp the essence and creativity to be successful. Through all my projects, I find a way to be innovative, efficient and positive towards myself and other group members. I am excited to continue my learning to be a well-rounded Software Engineer.<br></br>
+                </Text>
+            </Box>
+
+        </Flex>
+    )
+}
+
+export default About
---
+++ my-app/client/src/components/Contact.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import {
+    Label,
+    Input,
+    Textarea,
+} from '@rebass/forms'
+import {
+    Box, Flex, Button, Heading
+} from 'rebass';
+import axios from 'axios'
+
+class Contact extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+        console.log(e.target.value)
+    };
+
+    async handleSubmit(e) {
+        e.preventDefault()
+        const { name, email, message } = this.state
+
+        const form = await axios.post('/api/form', {
+            name,
+            email,
+            message
+        })
+    }
+
+    render() {
+        return (
+            <Box
+                as='form'
+                onSubmit={this.handleSubmit}
+                py={3}>
+                <Heading>Contact Me</Heading>
+                <hr></hr>
+                <Flex mx={-2} mb={3}>
+                    <Box width={3 / 4} px={2}>
+                        <Label htmlFor='name'>Name</Label>
+                        <Input
+                            id='name'
+                            name='name'
+                            onChange={this.handleChange}
+
+                        />
+                    </Box>
+                </Flex>
+
+                <Box mb={3} width={3 / 4}>
+                    <Label htmlFor='email'>Email</Label>
+                    <Input
+                        id='email'
+                        name='email'
+                        type='email'
+                        placeholder='jane@example.com'
+                        onChange={this.handleChange}
+
+                    />
+                </Box>
+
+                <Flex mx={-2} mb={3}>
+                    <Box width={3 / 4} px={2}>
+                        <Label htmlFor='location'>Comments</Label>
+                        <Textarea
+                            id='message'
+                            name='message'
+                            type='textarea'
+                            placeholder='comments'
+                            onChange={this.handleChange}
+                        >
+
+                        </Textarea>
+                    </Box>
+                </Flex>
+                <Flex mx={-2} flexWrap='wrap'>
+                    <Box px={2}>
+                        <Button variant='primary' mr={2}
+                            formValues={this.state}>
+                            Submit
+                        </Button>
+                    </Box>
+                </Flex>
+            </Box>
+        )
+    }
+}
+
+export default Contact
