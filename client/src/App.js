28369d5405c28c8037c763d1e409aafa036cfbb6
Carlo Ortega <cfor0@outlook.com>
2020-01-24 12:35:51 -0500

    added switch from react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import Projects from './components/Projects';
 
@@ -9,10 +9,12 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route exact path={'/'} component={Home}>
-          {/* <Route path={'home'} /> */}
-        </Route>     
-        <Route exact path={'/projects'} component={Projects}></Route>   
+        <Switch>
+          <Route exact path={'/'} component={Home}>
+            {/* <Route path={'home'} /> */}
+          </Route>
+          <Route exact path={'/projects'} component={Projects}></Route>
+        </Switch>
       </Router>
     );
   }
